ccd4290167119487f716c894140822fd88110576
Rolando <rolando.umana@gmail.com>
2019-11-15 00:13:24 -0700

    hellolambda function is using parameter store

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/roloum/hellolambda
 
 go 1.12
 
-require github.com/aws/aws-lambda-go v1.13.2
+require (
+	github.com/aws/aws-lambda-go v1.13.2
+	github.com/aws/aws-sdk-go v1.25.35
+)
--- hellolambda.go
+++ hellolambda.go
@@ -6,13 +6,33 @@ import (
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
 //Handler : Lambda handler invoked by lambda.Start
 func Handler() (string, error) {
+
+	ses, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")})
+	if err != nil {
+		return "{}", err
+	}
+
+	svc := ssm.New(ses)
+	paramName := "hellolambda.ParameterStore"
+	decryption := false
+	param, err := svc.GetParameter(&ssm.GetParameterInput{
+		Name:           &paramName,
+		WithDecryption: &decryption})
+	if err != nil {
+		return "{}", err
+	}
+
 	j, err := json.Marshal(map[string]interface{}{"message": fmt.Sprintf(
-		"Hello %v! Your function executed successfully!",
-		os.Getenv("HELLO_LAMBDA_MSG"))})
+		"Hello %v! %v!! Your function executed successfully!",
+		os.Getenv("HELLO_LAMBDA_MSG"), *param.Parameter.Value)})
 	if err != nil {
 		return "{}", err
 	}
